62f856df8cb0584aa892f764db8f39fd4691f67f
Chris Agostino <cjp.agostino@gmail.com>
2026-07-05 01:44:57 -0400

    fix stream export conflicts

--- src/core/index.ts
+++ src/core/index.ts
@@ -4,7 +4,46 @@ export * from './layout';
 export * from './files';
 export * from './utils';
 export * from './chat';
-export * from './stream';
+export {
+    StreamStatus,
+    StreamController,
+    StreamHandlers,
+    StreamEventType,
+    StreamConfig,
+    StreamEvent,
+    ToolCallDelta,
+    SSEChunk,
+    SSEChoice,
+    SSEDelta,
+    ToolExecutor,
+    ToolSchema,
+    ParsedChunk,
+    ResolvedTools,
+    ImageData,
+    TokenUsage,
+    cleanMessagesForLLM,
+    ensureSystemPrompt,
+    parseStreamChunk,
+    formatSSEEvent,
+    formatSSERaw,
+    resolveNPCTools,
+    executeTool,
+    accumulateToolCallDeltas,
+    createChatStream,
+    createToolAgentStream,
+    createJinxStream,
+    clean_messages_for_llm,
+    ensure_system_prompt,
+    parse_stream_chunk,
+    format_sse_event,
+    format_sse_raw,
+    resolve_npc_tools,
+    execute_tool,
+    _accumulate_tool_call_deltas,
+    create_chat_stream,
+    create_tool_agent_stream,
+    create_jinx_stream,
+} from './stream';
 export * from './browser';
 export * from './jobs';
 export * from './database';
